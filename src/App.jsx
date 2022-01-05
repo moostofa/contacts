de557905813dc5ef8f8571ea0d88a79169e914e6
moostofa <mmdrahinz96@gmail.com>
2022-01-06 00:42:52 +1100

    repo init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contacts from './components/Contacts';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello App</h1>
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contacts() {
+  return (
+    <div>
+      <h1>Hello Contacts</h1>
+    </div>
+  );
+}
+
+export default Contacts;
