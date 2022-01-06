7c503b7a7cfc3e3684d6b7930427109bdb988e0e
moostofa <mmdrahinz96@gmail.com>
2022-01-06 13:06:05 +1100

    created cards for each contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 import Contacts from './components/Contacts';
 
 function App() {
-  return (
-    <div>
-      <h1>Hello App</h1>
-      <Contacts />
-    </div>
-  );
+  return <Contacts />;
 }
 
 export default App;
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import {
+  Button, Box, Card, CardActions, CardContent, CardMedia, Typography,
+} from '@mui/material';
 
 function Contacts() {
   const [contacts, setContacts] = useState([{}]);
@@ -11,13 +14,43 @@ function Contacts() {
 
   useEffect(() => {
     getContacts();
-  });
+  }, []);
 
   return (
-    <div>
-      <h1>Hello Contacts</h1>
-      {JSON.stringify(contacts)}
-    </div>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexWrap="wrap"
+      gap={5}
+    >
+      {
+        contacts.map((contact) => (
+          <Card sx={{ width: 300 }} key={contact.username}>
+            <CardMedia
+              component="img"
+              height="150"
+              image="https://www.bigtopvoice.com/wp-content/uploads/2019/02/karlo-boni-1146386-unsplash.jpg"
+              alt="Profile picture"
+            />
+            <CardContent>
+              <Typography variant="h5" component="div">
+                { contact.name }
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Email:
+                {' '}
+                { contact.email }
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'space-between' }}>
+              <Button size="small" variant="contained" color="success">Call</Button>
+              <Button size="small" variant="contained" color="info">Edit details</Button>
+            </CardActions>
+          </Card>
+        ))
+      }
+    </Box>
   );
 }
 
