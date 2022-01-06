03f0b71aed684c8c5fe18b14e5297aaa92622b65
moostofa <mmdrahinz96@gmail.com>
2022-01-06 11:04:51 +1100

    successfully retrieve users from jsonplaceholder

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Contacts() {
+  const [contacts, setContacts] = useState([{}]);
+
+  const getContacts = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+    setContacts(users);
+  };
+
+  useEffect(() => {
+    getContacts();
+  });
+
   return (
     <div>
       <h1>Hello Contacts</h1>
+      {JSON.stringify(contacts)}
     </div>
   );
 }
